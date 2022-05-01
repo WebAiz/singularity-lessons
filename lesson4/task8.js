bcf98582eec0dc9be379f1350932c9f7fed92cf8
aizada <aizada.turarova@gmail.com>
2022-05-01 18:18:37 +0600

    Task 8

--- lesson4/task8.js
+++ lesson4/task8.js
@@ -1,17 +1,48 @@
 class Polling {
-	constructor(fn, delay, atempts) {}
-}
+  constructor(fn, delay, attempts) {
+    this.fn = fn;
+    this.delay = delay;
+    this.attempts = attempts;
+  }
 
+  async start() {
+    while (this.attempts > 0) {
+      await promiseAll([this.fn, this.delayedFn]);
+      this.attempts--;
+    }
+  }
+
+  delayedFn = () => {
+    return new Promise((res) => setTimeout(res, this.delay, "delayed"));
+  }
+}
+async function promiseAll(fns) {
+  const result = [];
+  const promises = [];
+  for (const fn of fns) {
+    const p = fn();
+    promises.push(p);
+  }
+
+  for (const p of promises) {
+    const res = await p;
+    result.push(res);
+  }
+
+  return result;
+}
 const wait = (delay) => new Promise((res) => setTimeout(res, delay));
 
 async function fn1() {
-	await wait(1000);
-	return Promise.resolve(1);
+  await wait(1000);
+  console.log("FN 1");
+  return Promise.resolve(1);
 }
 
 async function fn2() {
-	await wait(4000);
-	return Promise.resolve(1);
+  await wait(4000);
+  console.log("FN 2");
+  return Promise.resolve(1);
 }
 
 const poll = new Polling(fn1, 2000, 4);
